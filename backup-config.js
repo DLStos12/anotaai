bf7a51aab47c5117cac86133b460094c4ef1d85f
Derick Luiz <derick.stos@gmail.com>
2026-08-11 13:41:00 -0300

    Atualizar o backup-config.js

--- backup-config.js
+++ backup-config.js
@@ -1,5 +1,5 @@
 // Endereço público do arquivo backup.php instalado na SmileHost.
 // Exemplo: https://api.seudominio.com.br/anotaai/backup.php
-window.ANOTAAI_BACKUP_API = 'https://teste.acaidageh.shop/anotaai-api/backup.php';
-window.ANOTAAI_LICENSE_API = 'https://teste.acaidageh.shop/anotaai-api/license.php';
+window.ANOTAAI_BACKUP_API = 'https://teste.acaidageh.shop/smilehost-api/backup.php';
+window.ANOTAAI_LICENSE_API = 'https://teste.acaidageh.shop/smilehost-api/license.php';
 window.ANOTAAI_LICENSE_REQUIRED = true;
